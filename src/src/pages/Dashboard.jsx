891695a52387e2ac162de05f5ac053b465156af7
ryanmakau560-debug <ryanmakau560@gmail.com>
2026-04-28 08:56:50 +0300

    Update Dashboard.jsx

--- src/src/pages/Dashboard.jsx
+++ src/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import TaskForm from '../components/TaskForm';
 export default function Dashboard() {
   return (
     <div>
-      <h1>Project Lume Dashboard</h1>
+      <h1>Task manager Dashboard</h1>
       <TaskForm />
       <h2>Your Tasks</h2>
       {/* You will add the TaskList component here later */}
